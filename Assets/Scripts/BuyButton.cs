ba3271a21bc31ec49202d33fff72e2c29294c16e
cankcl2 <can.akcali2@gmail.com>
2023-11-25 14:12:02 -0500

    added some functionalities and scripts for UI

---
+++ Assets/Scripts/BuyButton.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuyButton : MonoBehaviour
+{
+    [HideInInspector]public int id;
+
+    //click func
+    public void BuyAnItem()
+    {
+        //buy the item
+    }
+}
---
+++ Assets/Scripts/ItemHolder.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ItemHolder : MonoBehaviour
+{
+    public Image itemImage;
+    //public BuyButton buyButton;
+    public TextMeshProUGUI itemNameText;
+    public TextMeshProUGUI gpsText;
+    public TextMeshProUGUI amountText;
+    public TextMeshProUGUI costText;
+}
